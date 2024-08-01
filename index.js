5fb206275ad35dde8332714d2412b89551f09786
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2024-08-01 14:52:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,168 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("WELCOME TO ADVENTURE GAME");
+class playerName {
+    name;
+    health = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    healthDecrease() {
+        let health = this.health - 25;
+        this.health = health;
+    }
+    healthIncrease() {
+        this.health = +100;
+    }
+}
+class opponentName {
+    name;
+    health = 100;
+    static select;
+    constructor(name) {
+        this.name = name;
+    }
+    healthDecrease() {
+        let health = this.health - 25;
+        this.health = health;
+    }
+}
+let player_Name = await inquirer.prompt([
+    {
+        name: "name",
+        type: "input",
+        message: "Please enter character",
+    }
+]);
+console.log(player_Name.name);
+let opponent = await inquirer.prompt([
+    {
+        type: "list",
+        name: "select",
+        message: "Select your opponent",
+        choices: ["Skeleton", "Assassin", "Zombie"]
+    }
+]);
+let p1 = new playerName(player_Name.name);
+let o1 = new opponentName(opponent.select);
+do {
+    if (opponent.select == "Skeleton") {
+        let move = await inquirer.prompt([
+            {
+                type: "list",
+                name: "opt",
+                message: "Select your opponent",
+                choices: ["Attack", "Drink Portion", "Run For Your Life...!"]
+            }
+        ]);
+        if (move.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.healthDecrease();
+                console.log(`${p1.name} fuel is ${p1.health}`);
+                console.log(`${o1.name} fuel is ${o1.health}`);
+                if (p1.health <= 0) {
+                    console.log("You Loose Better Luck Next Time....!");
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                o1.healthDecrease();
+                console.log(`${p1.name} fuel is ${p1.health}`);
+                console.log(`${o1.name} fuel is ${o1.health}`);
+                if (o1.health <= 0) {
+                    console.log("You Win...!");
+                    process.exit();
+                }
+            }
+        }
+        if (move.opt == "Drink Portion") {
+            p1.healthIncrease();
+            console.log(`Your Drink Health Portion Your Fuel is ${p1.health} `);
+        }
+        if (move.opt == "Run For Your Life...!") {
+            console.log("You Loose Better Luck Next Time");
+            process.exit();
+        }
+    }
+    //Assassain
+    if (opponent.select == "Assassin") {
+        let move = await inquirer.prompt([
+            {
+                type: "list",
+                name: "opt",
+                message: "Select your opponent",
+                choices: ["Attack", "Drink Portion", "Run For Your Life...!"]
+            }
+        ]);
+        if (move.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.healthDecrease();
+                console.log(`${p1.name} fuel is ${p1.health}`);
+                console.log(`${o1.name} fuel is ${o1.health}`);
+                if (p1.health <= 0) {
+                    console.log("You Loose Better Luck Next Time....!");
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                o1.healthDecrease();
+                console.log(`${p1.name} fuel is ${p1.health}`);
+                console.log(`${o1.name} fuel is ${o1.health}`);
+                if (o1.health <= 0) {
+                    console.log("You Win...!");
+                    process.exit();
+                }
+            }
+        }
+        if (move.opt == "Drink Portion") {
+            p1.healthIncrease();
+            console.log(`Your Drink Health Portion Your Fuel is ${p1.health} `);
+        }
+        if (move.opt == "Run For Your Life...!") {
+            console.log("You Loose Better Luck Next Time");
+            process.exit();
+        }
+    }
+    //Zombie
+    if (opponent.select == "Zombie") {
+        let move = await inquirer.prompt([
+            {
+                type: "list",
+                name: "opt",
+                message: "Select your opponent",
+                choices: ["Attack", "Drink Portion", "Run For Your Life...!"]
+            }
+        ]);
+        if (move.opt == "Attack") {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.healthDecrease();
+                console.log(`${p1.name} fuel is ${p1.health}`);
+                console.log(`${o1.name} fuel is ${o1.health}`);
+                if (p1.health <= 0) {
+                    console.log("You Loose Better Luck Next Time....!");
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                o1.healthDecrease();
+                console.log(`${p1.name} fuel is ${p1.health}`);
+                console.log(`${o1.name} fuel is ${o1.health}`);
+                if (o1.health <= 0) {
+                    console.log("You Win...!");
+                    process.exit();
+                }
+            }
+        }
+        if (move.opt == "Drink Portion") {
+            p1.healthIncrease();
+            console.log(`Your Drink Health Portion Your Fuel is ${p1.health} `);
+        }
+        if (move.opt == "Run For Your Life...!") {
+            console.log("You Loose Better Luck Next Time");
+            process.exit();
+        }
+    }
+} while (true);
